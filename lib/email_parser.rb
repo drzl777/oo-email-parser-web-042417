f2466b9994f6ec22189f0272ab211efa1cd44ffe
drzly777 <andrew.zheng725@gmail.com>
2017-04-30 20:36:03 -0400

    added email parsing but may have used too many instance variables

--- lib/email_parser.rb
+++ lib/email_parser.rb
@@ -1,4 +1,18 @@
-# Build a class EmailParser that accepts a string of unformatted 
+# Build a class EmailParser that accepts a string of unformatted
 # emails. The parse method on the class should separate them into
 # unique email addresses. The delimiters to support are commas (',')
 # or whitespace (' ').
+
+class EmailParser
+
+  attr_accessor :email_string, :email_array
+  def initialize(email_string)
+    self.email_string = email_string
+  end
+
+  def parse
+    email_split_by_csv = self.email_string.split(', ')
+    email_split_by_csv_and_space = email_split_by_csv.collect {|email_by_csv| email_by_csv.split}
+    self.email_array = email_split_by_csv_and_space.flatten.uniq
+  end
+end
